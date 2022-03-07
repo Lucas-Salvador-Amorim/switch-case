2feab27c14072e6223b29eba4efefa46e02cdd8c
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-07 19:41:57 +0000

    do while

---
+++ laçoDeRepetição.java
@@ -0,0 +1,12 @@
+public class laçoDeRepetição {
+    public static void main(String[] args){
+       int x = 20;
+
+       do{
+           System.out.println("Você não tem permissão para entrar");
+           x++;
+    } while (x < 10);
+
+}
+
+}
