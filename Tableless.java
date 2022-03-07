ec562b07f90e8767a76de7359092049168721788
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-07 19:17:59 +0000

    Aprendendo a usar o switch

---
+++ Tableless.java
@@ -0,0 +1,31 @@
+public class Tableless{
+    public static void main(String[] args){
+        int dia = 4;
+
+        switch(dia) {
+            case 1:
+            System.out.println("Segunda-feira");
+            break;
+
+            case 2:
+            System.out.println("Terça-feira");
+            break;
+
+            case 3:
+            System.out.println("Quarta-feira");
+            break;
+
+            case 4:
+            System.out.println("Quinta-feira");
+            break;
+
+            case 5:
+            System.out.println("Sexta-feira");
+            break;
+
+            default:
+            System.out.println("Nenhum dia da semana");
+
+        }
+    }
+}
